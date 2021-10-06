2ea531263fc7b9529535bdd469292302819d18a5
theson <junhyeon.son@lookpin.co.kr>
2021-10-06 17:16:20 +0900

    fix: validate - 하위 유효성 체크 함수가 import 되지 않던 문제 수정

--- packages/validate/fn.ts
+++ packages/validate/fn.ts
@@ -5,7 +5,7 @@ import { isNullable } from '../util/typeCheck';
  * undefined, null 여부도 확인하며, 값이 'undefined', 'null' 이라는 문자열로 된 것인지도 확인 한다.
  * @param val
  */
-export function validEmpty(val: string) {
+function empty(val: string) {
   return !isNullable(val) && !!val;
 }
 
@@ -15,8 +15,8 @@ export function validEmpty(val: string) {
  * @param min 최소 길이
  * @param max 최대 길이
  */
-export const validLengthCurried = (min: number, max: number) => (val: string) =>
-  validEmpty(val) && val.length >= min && val.length <= max;
+const length = (min: number, max: number) => (val: string) =>
+  empty(val) && val.length >= min && val.length <= max;
 
 /**
  * 이메일 유효성을 검증한다.
@@ -24,10 +24,9 @@ export const validLengthCurried = (min: number, max: number) => (val: string) =>
  * 내부적으로 validLength 를 수행하여 7~100 자 까지 인지를 함께 검증한다.
  * @param val
  */
-export function validEmail(val: string) {
+function email(val: string) {
   return (
-    validLengthCurried(7, 100)(val) &&
-    /^(\w|-|\.)+@\w+([.-]?\w+)*(\.\w{2,5})+$/.test(val)
+    length(7, 100)(val) && /^(\w|-|\.)+@\w+([.-]?\w+)*(\.\w{2,5})+$/.test(val)
   );
 }
 
@@ -37,7 +36,7 @@ export function validEmail(val: string) {
  * 총 길이가 6자~20자 까지 인지를 확인한다.
  * @param val
  */
-export function validPasswordWeak(val: string) {
+function passwordWeak(val: string) {
   //  숫자, 영문 대/소문자 및 특수문자가 모두 최소 1자 이상 포함되고
   // return (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,20})/).test(val);
   return /^(?=.*[A-z])(?=.*[0-9])(?=.{6,20})/.test(val);
@@ -49,7 +48,7 @@ export function validPasswordWeak(val: string) {
  * 총 길이가 6자~32자 까지 인지를 확인한다.
  * @param val
  */
-export function validPassword(val: string) {
+function password(val: string) {
   return /^(?=.*[A-z])(?=.*[~!@#$%^&*()\-=+_';<>/.`:"\\,[\]?|{}])(?=.*[0-9]).{6,32}$/.test(
     val
   );
@@ -59,47 +58,36 @@ export function validPassword(val: string) {
  * 소문자가 존재하는지 체크한다.
  * @param val
  */
-export function validLowerCase(val: string) {
+function lowercase(val: string) {
   return /^(?=.*[a-z])/.test(val);
 }
 /**
  * 대문자가 존재하는지 체크한다.
  * @param val
  */
-export function validUpperCase(val: string) {
+function uppercase(val: string) {
   return /^(?=.*[A-Z])/.test(val);
 }
 /**
  * 숫자가 존재하는지 체크한다.
  * @param val
  */
-export function validNumbers(val: string) {
+function numbers(val: string) {
   return /^(?=.*\d)/.test(val);
 }
 /**
  * 특수문자가 존재하는지 체크한다.
  * @param val
  */
-export function validSympols(val: string) {
+function symbols(val: string) {
   return /^(?=.*[~!@#$%^&*()\-=+_';<>/.`:"\\,[\]?|{}])/.test(val);
-}
-
-/**
- * 주민번호의 유효성을 체크한다.
- * @param val 체크할 주민번호
- */
-export function validJumin(val: string) {
-  // eslint-disable-next-line max-len
-  return /^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$/.test(
-    val
-  );
 }
 
 /**
  * 한글 또는 영문으로 2글자 이상, 100글자 이하인지 확인한다.
  * @param val 확인할 값
  */
-export function validKorEng(val: string) {
+function korAndEng(val: string) {
   return /^([A-Za-z]|[가-힣]){2,100}$/.test(val);
 }
 
@@ -108,18 +96,18 @@ export function validKorEng(val: string) {
  * 값은 번호만 이뤄져 있어야 하며 앞이 0으로 채워져 있어도 유효하다.
  * @param val 확인할 값
  */
-export function validNumberOnly(val: string) {
-  return !validEmpty(val) && /^\d{1,}$/.test(val);
+function numberOnly(val: string) {
+  return !empty(val) && /^\d{1,}$/.test(val);
   // isFinite(parseInt(val, 10));
 }
 
 /**
  * 전화번호 유효성을 체크 한다.
  *
- * 중간에 bar (-) 가 있어야 한다.
+ * 중간에 dash (-) 가 있어야 한다.
  * @param val 확인할 값
  */
-export function validPhoneWithBar(val: string) {
+function phoneWithDash(val: string) {
   return /^(\d{4}-\d{4}|\d{2,3}-\d{3,4}-\d{3,4})$/.test(val);
 }
 
@@ -127,17 +115,33 @@ export function validPhoneWithBar(val: string) {
  * 전화번호 유효성을 체크 한다.
  * 숫자로만 이뤄져 있어야하며, 최소 8자리, 최대 11자리 까지 허용 된다.
  *
- * 중간에 bar (-) 가 있어야 한다.
  * @param val 확인할 값
  */
-export function validPhone(val: string) {
-  return validLengthCurried(8, 11)(val) && validNumbers(val);
+function phone(val: string) {
+  return length(8, 11)(val) && numbers(val);
 }
 
 /**
  * 사업자 등록번호 유효성을 검증한다.
  * @param val
  */
-export function validCompanyRegNumber(val: string) {
+function companyRegNumber(val: string) {
   return /^\d{3}-\d{2}-\d{5}$/.test(val);
 }
+
+export const fn = {
+  empty,
+  email,
+  passwordWeak,
+  password,
+  lowercase,
+  uppercase,
+  numbers,
+  symbols,
+  korAndEng,
+  numberOnly,
+  phoneWithDash,
+  phone,
+  companyRegNumber,
+  length,
+};
