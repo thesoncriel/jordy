e1ba926ece807a5d0c9acdc2954c2d2a25d20198
theson <junhyeon.son@lookpin.co.kr>
2022-08-18 08:54:41 +0900

    feat: Axios 전용 Error Parser 함수 추가

---
+++ packages/http-api/throwableAxiosErrorParser.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpRestError } from '../types';
+import { isAxiosError } from './network.util';
+
+export function throwableAxiosErrorParser(error: any) {
+  if (isAxiosError(error)) {
+    const axiosErr = error;
+    const errorData = axiosErr.response?.data;
+    const meta = {
+      url: axiosErr.config.url || '',
+      status: axiosErr.response?.status || 0,
+      rawData: errorData,
+    };
+
+    if (HttpRestError.isErrorLike(errorData)) {
+      throw new HttpRestError(errorData.message, meta);
+    }
+    if (typeof errorData === 'string' && errorData) {
+      if (errorData.substring(0, 200).toLowerCase().includes('<html')) {
+        throw new HttpRestError(HttpRestError.DEFAULT_MESSAGE, meta);
+      }
+      throw new HttpRestError(errorData, meta);
+    }
+
+    throw new HttpRestError(HttpRestError.DEFAULT_MESSAGE, meta);
+  }
+}
