7332c7e60508cf990f282efae7f4ffebd83eabbd
theson <junhyeon.son@lookpin.co.kr>
2022-08-25 00:45:18 +0900

    feat: headerPipe 생성기 작성

---
+++ packages/http-api/createHttpHeaderPipe.test.ts
@@ -0,0 +1,142 @@
+import { TokenProvider, JWTProvider } from '../storage';
+import { setIsServer } from '../util/envCheck';
+import { createHttpHeaderPipe } from './createHttpHeaderPipe';
+
+describe('createHttpHeaderPipe', () => {
+  const TOKEN = 'bearer blah_blah';
+  const MSG_ERROR = 'login please';
+
+  const tokenProvider = {
+    type: 'TokenProvider',
+    get: vi.fn(() => TOKEN),
+    set: vi.fn(),
+    clear: vi.fn(),
+  };
+  const jwtProvider = {
+    type: 'JWTProvider',
+    refreshed: false,
+    pending: false,
+    accessToken: '',
+    refreshToken: '',
+    set: vi.fn(),
+    get: vi.fn().mockResolvedValue(TOKEN),
+    clear: vi.fn(),
+  };
+  const pipeMock1 = vi.fn((data: Map<string, string>) => {
+    data.set('name', 'lookpin');
+
+    return data;
+  });
+  const pipeMock2 = vi.fn((data: Map<string, string>) => {
+    data.set('age', '21');
+
+    return data;
+  });
+  const pipeMock3 = vi.fn((data: Map<string, string>, token = '') => {
+    if (token) {
+      data.set('token', token);
+    }
+    return data;
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe.each([
+    ['TokenProvider', tokenProvider],
+    ['JWTProvider', jwtProvider],
+  ])('%s 적용 테스트', (_, provider) => {
+    const headerPipe = createHttpHeaderPipe(
+      provider as TokenProvider | JWTProvider,
+      MSG_ERROR
+    );
+
+    it('적용된 모든 오퍼레이터들을 1번씩 수행한다.', async () => {
+      await headerPipe(pipeMock1, pipeMock2, pipeMock3);
+
+      expect(pipeMock1).toBeCalledTimes(1);
+      expect(pipeMock2).toBeCalledTimes(1);
+      expect(pipeMock3).toBeCalledTimes(1);
+    });
+
+    it('만들어진 헤더는 오퍼레이터에서 제공된 내용을 기반으로 만들어진다.', async () => {
+      pipeMock1.mockImplementationOnce((data) => {
+        data.set('gameName', 'sonic3');
+
+        return data;
+      });
+      pipeMock2.mockImplementationOnce((data) => {
+        data.set('genre', 'action');
+
+        return data;
+      });
+      const result = await headerPipe(pipeMock1, pipeMock2);
+
+      expect(result).toEqual({
+        gameName: 'sonic3',
+        genre: 'action',
+      });
+    });
+
+    it('오퍼레이터를 주지 않으면 빈 객체를 만든다.', async () => {
+      const result = await headerPipe();
+
+      expect(result).toEqual({});
+    });
+
+    it('헤더 생성 시 적용된 토큰 제공자를 이용한다.', async () => {
+      await headerPipe(pipeMock1, pipeMock2, pipeMock3);
+
+      expect(provider.get).toBeCalledTimes(1);
+    });
+
+    it('생성된 헤더에 토큰 제공자의 토큰값이 포함된다.', async () => {
+      const token = 'some auth token';
+
+      if (provider.type === 'JWTProvider') {
+        provider.get.mockResolvedValueOnce(token);
+      } else {
+        provider.get.mockImplementationOnce(() => token);
+      }
+
+      const result = await headerPipe(pipeMock1, pipeMock2, pipeMock3);
+
+      expect(result).toHaveProperty('token', token);
+    });
+
+    it('토큰 제공자의 토큰값이 비었다면 오류를 일으킨다.', async () => {
+      setIsServer(false);
+
+      if (provider.type === 'JWTProvider') {
+        provider.get.mockResolvedValueOnce('');
+      } else {
+        provider.get.mockImplementationOnce(() => '');
+      }
+
+      await expect(
+        headerPipe(pipeMock1, pipeMock2, pipeMock3)
+      ).rejects.toThrowError(MSG_ERROR);
+
+      setIsServer(true);
+    });
+
+    it('토큰 제공자에서 오류가 발생되면 그 오류 내용을 그대로 전달한다.', async () => {
+      let msg = '';
+
+      if (provider.type === 'JWTProvider') {
+        msg = 'oops!!';
+        provider.get.mockRejectedValueOnce(new Error(msg));
+      } else {
+        msg = 'what?!';
+        provider.get.mockImplementationOnce(() => {
+          throw new Error(msg);
+        });
+      }
+
+      await expect(
+        headerPipe(pipeMock1, pipeMock2, pipeMock3)
+      ).rejects.toThrowError(msg);
+    });
+  });
+});
---
+++ packages/http-api/createHttpHeaderPipe.ts
@@ -0,0 +1,78 @@
+/* eslint-disable no-param-reassign */
+import { JWTProvider, TokenProvider } from '../storage';
+import { isServer } from '../util/envCheck';
+
+type HeaderFieldMaker = (
+  defHeader: Map<string, string>,
+  token?: string
+) => Map<string, string>;
+
+function makeHeaders(pipes: HeaderFieldMaker[], token = '') {
+  const rawMap = pipes.reduce(
+    (prev, fn) => fn(prev, token),
+    new Map<string, string>()
+  );
+
+  return Object.fromEntries(rawMap) as Record<string, string>;
+}
+
+/**
+ * HTTP 헤더를 제공한다.
+ *
+ * tokenProvider 설정 후 operator 로 필요한 헤더 요소를 함수로 구성하여
+ * 다양한 HTTP 헤더를 만들 수 있다.
+ *
+ * ```ts
+ * import {
+ *   createTokenProvider,
+ *   createHttpHeaderPipe,
+ *   httpHeaderOperator,
+ * } from 'jordy';
+ *
+ * const tokenProvider = createTokenProvider('local', 'myValue');
+ * const headerPipe = createHttpHeaderPipe(tokenProvider);
+ * const headers = headerPipe(
+ *   // 미리 제공된 오퍼레이터 사용
+ *   httpHeaderOperator.contentTypeJson,
+ *   httpHeaderOperator.bearerToken,
+ *   // 직접 오퍼레이터 작성
+ *   (data: Map<string, string>, token: string) => {
+ *     data.set('its', 'my-life-' + token);
+ *
+ *     return data;
+ *   },
+ * );
+ *
+ * console.log(headers);
+ * ```
+ * ```json
+ * {
+ *   "Content-Type": "application/json; charset=utf-8",
+ *   "Authorization": "Bearer some_token_value",
+ *   "its": "my-life-some_token_value"
+ * }
+ * ```
+ *
+ * @param provider 토큰 제공자.
+ * @return {Function} 오퍼레이터를 연속으로 사용하여 헤더 객체를 만드는 파이프 함수.
+ * @see TokenProvider
+ * @see JWTProvider
+ */
+export const createHttpHeaderPipe = (
+  provider?: TokenProvider | JWTProvider,
+  loginRequiredMessage = '로그인 상태가 만료 되었습니다.\n다시 로그인 하여 주시기 바랍니다.'
+) => {
+  return async function headerPipe(...pipes: HeaderFieldMaker[]) {
+    let token = '';
+
+    if (provider) {
+      token = await provider.get();
+
+      if (!token && !isServer()) {
+        throw new Error(loginRequiredMessage);
+      }
+    }
+
+    return makeHeaders(pipes, token);
+  };
+};
