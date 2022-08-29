2da6a4cdd07410c3f8a0e2e35ca1147b3ba6ae7b
theson <junhyeon.son@lookpin.co.kr>
2022-08-29 20:02:48 +0900

    feat: 기존 HttpApi 팩토리에 interceptor 설정 가능한 인터페이스를 반환토록 반환타입 명시

--- packages/http-api/createHttpUploadApi.factory.ts
+++ packages/http-api/createHttpUploadApi.factory.ts
@@ -1,17 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { qs } from '../util';
 import { AxiosHttpUploadProvider } from './axios';
 import { BasicHttpUploadApi } from './BasicHttpUploadApi';
-import { HttpUploadApi } from './network.type';
+import { InterceptorHttpUploadApi } from './network.type';
 import { defaultHeaderCreator } from './network.util';
 
 export function createHttpUploadApi(
   baseUrl: string,
   headerCreator: () => Promise<Record<string, string>> = defaultHeaderCreator,
+  paramsSerializer: (params: any) => string = qs.serialize,
   withCredentials = true
-): HttpUploadApi {
+): InterceptorHttpUploadApi {
   return new BasicHttpUploadApi(
     new AxiosHttpUploadProvider(),
     baseUrl,
     headerCreator,
+    paramsSerializer,
     withCredentials
   );
 }
